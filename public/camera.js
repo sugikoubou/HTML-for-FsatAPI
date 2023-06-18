db3c2f00016a3b9d75643f7119a250e168d66bd0
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-06-19 07:01:22 +0900

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -50,9 +50,36 @@ window.onload = () => {
       body: {file: test}
     };
      
-    sendServer('https://a13a-2400-4150-4341-1f00-b562-68d1-b7c1-fd5e.ngrok-free.app/api/predict', param);
+    sendServer('https://7035-2400-4150-4341-1f00-b562-68d1-b7c1-fd5e.ngrok-free.app/api/predict', param);
+
+
+    });
+/**
+ * サーバへJSON送信
+ *
+ * @param url   {string} 送信先URL
+ * @param param {object} fetchオプション
+ */
+  function sendServer(url, param){
+    fetch(url, param)
+      .then((response)=>{
+        return response.json();
+      })
+      .then((json)=>{
+        if(json.status){
+          alert("送信に『成功』しました");
+          setImage(json.result);    //json.resultにはファイル名が入っている
+        }
+        else{
+          alert("送信に『失敗』しましたよ");
+          console.log(`[error1] ${json.result}`);
+          }
+      })
+      .catch((error)=>{
+        alert("送信に『失敗』しました");
+        console.log(`[error2] ${error}`);
+        });
+    }
      
-    alert("UGEEEEEEEEEE");
-     
-  });
+  
 };
