3c86ad59997e1c90f78a0930dec80aae4ac9dfc6
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-06-19 06:45:38 +0900

    Update テストボタン.js

--- public/テストボタン.js
+++ public/テストボタン.js
@@ -4,7 +4,13 @@ const Banner = {
   fontcolor: "Blue",   // 文字色
   text: "Hello World", // テキスト
 
-
+  // Canvas情報
+  canvas: {
+    width: null,   // 横幅
+    height: null,  // 高さ
+    ctx: null      // context
+  }
+}
 
 //---------------------------------------------
 // [event] ページ読み込み完了
@@ -23,24 +29,23 @@ window.onload = ()=>{
   Banner.drawImage(img, 0, 0, canvas.width, canvas.height);
 
 
-const button = document.querySelector('input');
+  const button = document.querySelector('input');
 
-button.addEventListener('click', updateButton);
+  button.addEventListener('click', updateButton);
 
-function updateButton() {
-  if (button.value === 'クリックしてね') {
-    button.value = 'マシンを停止';
+  function updateButton() {
+    if (button.value === 'クリックしてね') {
+      button.value = 'マシンを停止';
 
-    const canvas = board.toDataURL("image/jpg");  // DataURI Schemaが返却される
+      const canvas = board.toDataURL("image/jpg");  // DataURI Schemaが返却される
 
 
-    const param  = {
-      method: "POST",
-      body: canvas
-    };
+      const param  = {
+        method: "POST",
+        body: canvas
+      };
     
-    sendServer('https://7035-2400-4150-4341-1f00-b562-68d1-b7c1-fd5e.ngrok-free.app/api/predict', param);
-
+      sendServer('https://7035-2400-4150-4341-1f00-b562-68d1-b7c1-fd5e.ngrok-free.app/api/predict', param);
 
 /**
  * サーバへJSON送信
@@ -48,29 +53,29 @@ function updateButton() {
  * @param url   {string} 送信先URL
  * @param param {object} fetchオプション
  */
-function sendServer(url, param){
-  fetch(url, param)
-    .then((response)=>{
-      return response.json();
-    })
-    .then((json)=>{
-      if(json.status){
-        alert("送信に『成功』しました");
-        setImage(json.result);    //json.resultにはファイル名が入っている
-      }
-      else{
-        alert("送信に『失敗』しましたよ");
-        console.log(`[error1] ${json.result}`);
-      }
-    })
-    .catch((error)=>{
-      alert("送信に『失敗』しました");
-      console.log(`[error2] ${error}`);
-    });
-}
+  function sendServer(url, param){
+    fetch(url, param)
+      .then((response)=>{
+        return response.json();
+      })
+      .then((json)=>{
+        if(json.status){
+          alert("送信に『成功』しました");
+          setImage(json.result);    //json.resultにはファイル名が入っている
+        }
+        else{
+          alert("送信に『失敗』しましたよ");
+          console.log(`[error1] ${json.result}`);
+          }
+      })
+      .catch((error)=>{
+        alert("送信に『失敗』しました");
+        console.log(`[error2] ${error}`);
+        });
+    }
 
     
+    } 
   } 
-}
 
 }
