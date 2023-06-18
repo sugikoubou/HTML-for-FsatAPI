5c77c3b4b40aa3147bfbafa84de7df901f8ed230
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-06-19 01:20:50 +0900

    Update テストボタン.js

--- public/テストボタン.js
+++ public/テストボタン.js
@@ -6,5 +6,47 @@ function updateButton() {
   if (button.value === 'クリックしてね') {
     button.value = 'マシンを停止';
     alert("AAAAAA");
+
+    const canvas = 
+
+    const param  = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({data: canvas})
+    };
+    
+    sendServer('https://a13a-2400-4150-4341-1f00-b562-68d1-b7c1-fd5e.ngrok-free.app/api/predict', param);
+
+
+/**
+ * サーバへJSON送信
+ *
+ * @param url   {string} 送信先URL
+ * @param param {object} fetchオプション
+ */
+function sendServer(url, param){
+  fetch(url, param)
+    .then((response)=>{
+      return response.json();
+    })
+    .then((json)=>{
+      if(json.status){
+        alert("送信に『成功』しました");
+        setImage(json.result);    //json.resultにはファイル名が入っている
+      }
+      else{
+        alert("送信に『失敗』しました");
+        console.log(`[error1] ${json.result}`);
+      }
+    })
+    .catch((error)=>{
+      alert("送信に『失敗』しました");
+      console.log(`[error2] ${error}`);
+    });
+}
+
+    
   } 
 }
