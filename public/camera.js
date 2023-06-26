1cff179b79974140ce3aa19ce746ad128af2a445
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-06-26 15:25:11 +0900

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const video  = document.querySelector("#camera");
   const canvas = document.querySelector("#picture");
-  let URL = "https://f951-158-217-53-250.ngrok-free.app//api/predict"
+  let URL = "https://f951-158-217-53-250.ngrok-free.app/api/predict"
 
   /** カメラ設定 */
   const constraints = {
