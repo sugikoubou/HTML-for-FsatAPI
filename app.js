4b7eb22351ee77c45f32095a8673ac75c0dfb94b
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-07-03 03:17:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var options = {
   dotfiles: 'ignore',
   etag: false,
   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['indexAR.html'],   //ここでどれにするか指定？
+  index: ['index.html'],   //ここでどれにするか指定？
   maxAge: '1m',
   redirect: false
 }
