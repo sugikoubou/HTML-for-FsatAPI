12dcace26977d75bdb58864547ae9932e81a8e48
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-07-03 05:42:51 +0900

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -56,14 +56,20 @@ window.onload = () => {
     const imgs = ['info/1/syouki.jpg', 'info/2/syouki.jpg', 'info/3/syouki.jpg','https://tblg.k-img.com/restaurant/images/Rvw/180968/640x640_rect_150a2a04531912b3b9067794ce5866e4.jpg'];
     var info = new Image();
     info.src = imgs[0];
-    ctx.drawImage(info, 0, 0, canvas.width/4, canvas.height/4);
-    /*
+    info.onload = () => {
+      ctx.drawImage(info, 0, 0, canvas.width/4, canvas.height/4);
+    };
+
     info.src = imgs[3];
-    ctx.drawImage(info, 600, 600, canvas.width/4, canvas.height/4);
-    */
+    info.onload = () => {
+      ctx.drawImage(info, 600, 600, canvas.width/4, canvas.height/4);
+    };
     
 
   });
+
+
+  
 
   function POST(imgNE) {
     const data = imgNE
