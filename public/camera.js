66d85b11aeb9ac6b4ee960a9c00e78c96c3b8154
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-07-03 04:01:47 +0900

    ちょっと関数いじった

--- public/camera.js
+++ public/camera.js
@@ -1,24 +1,10 @@
 window.onload = () => {
   const video  = document.querySelector("#camera");
   const canvas = document.querySelector("#picture");
-  let URL = "https://cdae-2400-4150-4341-1f00-24b4-1ef5-342a-3462.ngrok-free.app/api/predict"
+  let URL = "https://93d0-2400-4150-4341-1f00-940b-675b-8e2e-40c7.ngrok-free.app/api/predict"
 
 
-  function get_func(url) {
-      fetch(url)
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(text) {
-        let view = document.getElementById("view")
-        view.textContent = ""
-        // 取得テキストを一行ごとにループ（ただ改行して表示しているだけ）
-        text.split("\n").forEach((value) => {
-          view.insertAdjacentHTML('beforeend', value);
-          view.insertAdjacentHTML('beforeend', "<br>");
-        })
-      });
-    }
+  
   
   /** カメラ設定 */
   const constraints = {
@@ -64,7 +50,13 @@ window.onload = () => {
     // canvasをpng
 　　let data = canvas.toDataURL("image/png");
     
-    const filename="sample.png";
+    POST();
+
+});
+
+  function POST() {
+    
+　　const filename="sample.png";
     const type="image/png";
     const bin = atob(data.replace(/^.*,/, ''));
     const buffer = new Uint8Array(bin.length).map((_,x)=>bin.charCodeAt(x));
@@ -78,25 +70,24 @@ window.onload = () => {
       res.json()).then(get_func)
       
       //.then(console.log)
+    
+  　}
 
-  
-
-});
-
-  function _convertToFile (imgData) {
-　　　　　  let data = imgData;
-            const filename="sample.png";
-            const type="image/png";
-            const bin = atob(data.replace(/^.*,/, ''));
-            const buffer = new Uint8Array(bin.length).map((_,x)=>bin.charCodeAt(x));
-            const blob = new Blob([buffer.buffer], {type});
-            const method="post";
-            const body=new FormData();
-            body.append("file",blob,filename);
-            fetch(URL,{method,body}).then(res=>res.text()).then(console.log);
-        }
-
-  
+  function get_func(url) {
+      fetch(url)
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(text) {
+        let view = document.getElementById("view")
+        view.textContent = ""
+        // 取得テキストを一行ごとにループ（ただ改行して表示しているだけ）
+        text.split("\n").forEach((value) => {
+          view.insertAdjacentHTML('beforeend', value);
+          view.insertAdjacentHTML('beforeend', "<br>");
+        })
+      });
+    }
 
   
 };
