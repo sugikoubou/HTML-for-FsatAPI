d5d02159a0c4eba77f7a6590451b3bc6742689e2
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-08-09 17:32:59 +0900

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -110,7 +110,7 @@ window.onload = () => {
     //.then(res=>res.json()).then(console.log)
     fetch(URL,{method,body})
     .then(res=>res.json())
-      //.then(get_func)
+      .then(get_func)
       
       .then(console.log)
     
