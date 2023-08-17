e2eca8ed5028af5b6cf09d2290a7acd3bdcf4222
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-08-17 23:02:56 +0900

    Update camera.js

--- public/camera.js
+++ public/camera.js
@@ -39,6 +39,26 @@ window.onload = () => {
   });
 
   //シャッターボタンが押されたらの動作が下
+  document.getElementById("doom").addEventListener("click", function(){
+	//ここに処理を書く
+  const ctx = canvas.getContext("2d");
+
+    // 演出的な目的で一度映像を止めてSEを再生する
+    video.pause();  // 映像を停止
+    setTimeout( () => {
+      video.play();    // 0.5秒後にカメラ再開
+    }, 500);
+
+    // canvasに画像を貼り付ける
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+     
+    // canvasをpng
+　　const data = canvas.toDataURL("image/png");
+    
+    POST(data);
+}, false);
+
+  
    document.querySelector("#shutter").addEventListener("click", () => {
     const ctx = canvas.getContext("2d");
 
