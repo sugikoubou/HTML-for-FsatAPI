a44aea37d10c8b57254d2f31f2c1b4c5e682c870
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-08-18 04:07:50 +0900

    Update IfProcess.js

--- public/IfProcess.js
+++ public/IfProcess.js
@@ -2,7 +2,7 @@ export default function resCheck(respo) {
   console.log(respo);
   const test = JSON.stringify(respo);
   console.log(test);
-  test2 = test.replace('[', '').replace(']', '');
+  const test2 = test.replace('[', '').replace(']', '');
   console.log(test2,test2.Credit);
   
   if(test.Name === 'koiwa'){
