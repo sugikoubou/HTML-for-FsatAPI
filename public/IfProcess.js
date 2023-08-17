3cc900abdf8f859bf4c8c02cd51c510e45ca4925
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-08-18 00:51:17 +0900

    Update IfProcess.js

--- public/IfProcess.js
+++ public/IfProcess.js
@@ -2,4 +2,12 @@ export default function resCheck(name) {
   if(name === 'koiwa'){
       console.log('ここは小岩');
   }
+  if (condition1)
+    statement1
+  else if (condition2)
+    statement2
+  else if (condition3)
+    statement3
+  else
+    statementN
 }
