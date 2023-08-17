2fa8c9b420c1dc1b5fc89238ad9645588f3a7ecc
sugikoubou <70837532+sugikoubou@users.noreply.github.com>
2023-08-18 04:33:21 +0900

    Update IfProcess.js

--- public/IfProcess.js
+++ public/IfProcess.js
@@ -6,14 +6,23 @@ export default function resCheck(respo) {
   if(respo.Name === 'koiwa'){
       console.log('ここは小岩');
   }
-  /*
-  if (condition1)
-    statement1
-  else if (condition2)
-    statement2
-  else if (condition3)
-    statement3
-  else
-    statementN
-    */
+  
+  else if (respo.Name ==='kizan'){
+
+    
+  }
+  
+  else if (respo.Name === 'nukunuku'){
+
+    
+  else if (respo.Name === 'nakanoya'){
+
+    
+  else if (respo.Name === 'syouki'){
+  }
+
+  else{
+    console.log
+
+    
 }
